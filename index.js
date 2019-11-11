b5ec2fa92cd5d1e404e4eff6bc1a09495b620918
Finny Abraham <kozakk@gmail.com>
2019-11-11 03:12:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-var kittens = [] //define your array here
+var kittens = ['Milo','Otis','Garfield'] //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+destructivelyRemoveLastKitten() {
+  kittens.pop()
+}
+destructivelyRemoveFirstKitten() {
+  kittens.shift()
+}
