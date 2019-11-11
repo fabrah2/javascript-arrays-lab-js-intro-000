557007a8fd32a4a474079dbaf4b6ca388799f09c
Finny Abraham <kozakk@gmail.com>
2019-11-11 03:27:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function prependKitten(name) {
   return [name, ...kittens]
 }
 function removeLastKitten() {
-  return kittens.splice(0,kittens.length - 1)
+  return kittens.slice(0,kittens.length - 1)
 }
 function removeFirstKitten() {
-  return kittens.splice(1)
+  return kittens.slice(1)
 }
